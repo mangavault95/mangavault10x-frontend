e2de99f1346d450689b61302979474fbb9076285
MangaVault10x <carmine.montella.arcaplanet@gmail.com>
2026-05-14 13:00:14 +0200

    Change manga API endpoint to new URL
    Updated the API endpoint to fetch manga data from a new source.

--- src/components/MangaGrid.jsx
+++ src/components/MangaGrid.jsx
@@ -7,7 +7,7 @@ export default function MangaGrid({ search = "", filter = "all" }) {
   const [selectedManga, setSelectedManga] = useState(null);
 
   useEffect(() => {
-    fetch("http://localhost:3001/api/manga")
+    fetch("https://mangavault10x-api.onrender.com/api/manga")
       .then((res) => res.json())
       .then((data) => setManga(Array.isArray(data) ? data : []))
       .catch(() => setManga([]));
