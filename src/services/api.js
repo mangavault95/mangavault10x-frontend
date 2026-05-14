1fdd0a8b8e192090a5e5aece4387216ff2518ef3
MangaVault10x <carmine.montella.arcaplanet@gmail.com>
2026-05-14 13:45:13 +0200

    Update API_URL to use environment variable

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3001/api";
+const API_URL = "fetch(`${import.meta.env.VITE_API_URL}/api`)";
 
 export async function getManga() {
   const res = await fetch(`${API_URL}/manga`);
