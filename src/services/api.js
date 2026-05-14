e718a76a49a3d2665d4c8399a7a018cb62b21027
MangaVault10x <carmine.montella.arcaplanet@gmail.com>
2026-05-14 14:13:42 +0200

    Refactor API functions to use API object

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,5 @@
-const API_URL = import.meta.env.VITE_API_URL;
+const API_BASE = import.meta.env.VITE_API_URL;
 
-export async function getManga() {
-  const res = await fetch(`${API_URL}/api/manga`);
-  return res.json();
-}
+export const API = {
+  getManga: () => fetch(`${API_BASE}/api/manga`).then(r => r.json())
+};
